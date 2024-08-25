5fb76dd4755ba04f375e6af0af0c793028aa8af6
aerochops <150325960+aerochops@users.noreply.github.com>
2024-08-25 23:00:26 +0700

    menambahkan detail dan routing

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -43,7 +43,19 @@ class PostController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $posts = Storage::get('posts.txt');
+        $posts = explode("\n", $posts);
+        $selected_post = array();
+        foreach ($posts as $post) {
+            $post = explode(",", $post);
+            if ($post[0] == $id) {
+                $selected_post = $post;
+            }
+        }
+        $view_data = [
+            'post' => $selected_post,
+        ];
+        return view('posts.show', $view_data);
     }
 
     /**
---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog| judul: {{$post[1]}}</title>
+    <link rel="stylesheet" href="{{asset('bootstrap-5/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/blog.css')}}">
+    <script src="{{asset('bootstrap-5/js/bootstrap.bundle.min.js')}}"></script>
+</head>
+
+<body>
+    <div class="container">
+        <article class="blog-post">
+            <h2 class="display-5 link-body-emphasis mb-1">{{$post[1]}}</h2>
+            <p class="blog-post-meta">{{date("D M Y H:i",strtotime($post[3]))}}</p>
+
+            <p>{{$post[2]}}</p>
+        </article>
+
+        <a href="{{url("posts")}}">
+            < Kembali
+                </a>
+    </div>
+</body>
+
+</html>
