7bf4feed00828697bbc737c02a556acc308f0f04
aerochops <150325960+aerochops@users.noreply.github.com>
2024-08-31 00:22:56 +0700

    Pembuatan form tambah data

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -23,7 +23,10 @@
 
 
     <div class="container">
-        <h1>Blog Codepolitan</h1>
+        <h1>
+            Blog Codepolitan
+            <a class="btn btn-success" href="{{url('posts/create')}}">+ Buat Postingan</a>
+        </h1>
         @foreach($posts as $post)
         @php($post=explode(",",$post))
         <div class="card mb-3">
@@ -31,7 +34,7 @@
                 <h5 class="card-title">{{$post[1]}}</h5>
                 <p class="card-text">{{$post[2]}}</p>
                 <p class="card-text"><small class="text-body-secondary">Last updated at {{date("d M Y H:i",strtotime($post[3])) }}</small></p>
-                <a href="{{url("posts/$post[0]")}}" class="btn btn-primary">Selengkapnya</a>
+                <a href="{{url('posts/$post[0]')}}" class="btn btn-primary">Selengkapnya</a>
             </div>
         </div>
 
