b56eafc869f432fb808bb6d8d281a0dde018b2cf
aerochops <150325960+aerochops@users.noreply.github.com>
2024-09-16 22:52:37 +0700

    menghapus file add_active_to_posts dan menambahkan active di create_posts_table

--- database/migrations/2024_09_06_005245_create_posts_table.php
+++ database/migrations/2024_09_06_005245_create_posts_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title', 100);
             $table->text('content');
+            $table->boolean('active')->default(true);
             $table->timestamps();
         });
     }
